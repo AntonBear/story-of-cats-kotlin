62f721f40593e137535adfc224b5c8ce6c2fd335
ANTON-PC <holzed15@gmail.com>
2024-10-14 00:21:42 +0500

    feat: refactor JSON

--- app/src/main/java/models/Button.kt
+++ app/src/main/java/models/Button.kt
@@ -4,9 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Button (
-    val place: Int?,
     val text: String?,
-    val nameOfNextScene: String?,
     val idOfNextScene: Int?
 
 )
